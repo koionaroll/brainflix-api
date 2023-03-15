fe71c5f026a54e5d4d8b6659ea1677162c485446
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-15 14:58:38 -0700

    fixed upload bugs

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,20 +1,44 @@
-// app.post((req, res) => {
+const express = require("express");
+const router = express.Router();
+const vidData = require("../data/video-details.json");
+const fs = require("fs");
+const uuid = require("uuid");
 
-//   const body = req.body;
-//   const title = body.title;
-//   const channel = body.channel;
-//   const image = body.image;
+function readUpload() {
+  const vidFile = fs.readFileSync("./data/video-details.json");
+  const vidInfo = JSON.parse(vidFile);
+  return vidInfo;
+}
 
-//   const newVid = {
-//   id: uuid.v4(),
-//   title,
-//   channel,
-//   image,
-//   description,
+function writeUpload(data) {
+  const stringifiedData = JSON.stringify(data);
+  fs.writeFileSync("./data/video-details.json", stringifiedData);
+}
 
-// }
+router.post("/", (req, res) => {
+  const vid = readUpload();
 
-// vidData.push(newVid)
-// res.status(201).json(vidData);
+  const body = req.body;
+  const title = body.title;
+  const description = body.description;
 
-// });
+  const newVid = {
+    id: uuid.v4(),
+    title:title,
+    channel: "New User",
+    image: "//localhost:8080/assets/images/Upload-video-preview.jpg",
+    description: description,
+    views: "0",
+    likes: "0",
+    duration: "10:00",
+    video: "",
+    timestamp: Date.now(),
+    comments: [],
+  };
+
+  vid.push(newVid);
+  writeUpload(vid);
+  res.status(201).send();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const uuid = require("uuid");
 const app = express();
 const cors = require("cors")
 const videoRoute = require("./routes/videoRoute");
+const uploadRoute = require("./routes/uploadRoute")
 
 require("dotenv").config()
 const port = process.env.PORT
@@ -14,6 +14,7 @@ app.use(cors())
 
 
 app.use('/', videoRoute);
+app.use('/UploadPage', uploadRoute);
 
 
 
